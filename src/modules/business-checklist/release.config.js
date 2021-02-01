d8df42092c60f5db0b7d40b52e923847bee2b10d
Frank Pagan <frank@cocreate.app>
2021-02-01 18:40:35 +0000

    clean file tree

---
+++ src/modules/business-checklist/release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  dryRun: false,
+  branches: ["master", "next"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+      },
+    ],
+  ],
+};
