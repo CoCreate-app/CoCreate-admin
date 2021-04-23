77a110dabd27210c9f89ded39e5716f0b4cb3837
frank <frank@cocreate.app>
2021-04-23 22:10:15 +0000

    fix: readme and documentation. Removed securitykeys

--- dist/apikey.js
+++ dist/apikey.js
@@ -8,7 +8,6 @@
     
     var config = {
         apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
-        securityKey: 'f26baf68-e3a9-45fc-effe-502e47116265',
         organization_Id: '5de0387b12e200ea63204d6c',
         // host: 'wss://ws.cocreate.app'
       }
