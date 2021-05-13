55026cc59d953b86b2dd276751a200b01f356b91
frank <frank@cocreate.app>
2021-05-13 01:34:26 +0000

    Update docs, readme, package.json

---
+++ CoCreate.config.js
@@ -0,0 +1,85 @@
+module.exports = {
+    "config": {
+        "apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
+        "securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
+        "organization_Id": "5de0387b12e200ea63204d6c",
+        "host": "server.cocreate.app:8088"
+    },
+    "sources": [
+        {
+            "entry": "./src/index.html",
+            "collection": "files",
+            "document_id": "60971d51624235187e4d695b",
+            "key": "src",
+            "data": {
+                "name": "AdminUI",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/admin"
+            }
+        },
+        {
+            "entry": "./src/signin.html",
+            "collection": "files",
+            "document_id": "60971dd3624235187e4d695e",
+            "key": "src",
+            "data": {
+                "name": "AdminUI signin",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/signin"
+            }
+        },
+        {
+            "entry": "./src/signup.html",
+            "collection": "files",
+            "document_id": "60971f5b624235187e4d6965",
+            "key": "src",
+            "data": {
+                "name": "AdminUI Signup",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/signup"
+            }
+        },
+        {
+            "entry": "./src/builder.html",
+            "collection": "files",
+            "document_id": "60972231624235187e4d6978",
+            "key": "src",
+            "data": {
+                "name": "Builder",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/builder"
+            }
+        },
+        {
+            "entry": "./src/index.css",
+            "collection": "files",
+            "document_id": "60972231624235187e4d6979",
+            "key": "src",
+            "data": {
+                "name": "AdminUI CSS",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/admin/index.css"
+            }
+        }
+    ]
+}
