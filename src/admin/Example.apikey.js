5e59e2437e21d8743d2f27749dab9d3e667173fe
Frank Pagan <frank@cocreate.app>
2021-10-14 18:02:00 +0000

    fix: added notigfication template and example.apiKey

---
+++ src/admin/Example.apikey.js
@@ -0,0 +1,5 @@
+var config = {                 
+    apiKey: '', // Your organization_id can be retrived after signing up at https://cocreate.app              
+    organization_Id: '', // Your organization_id can be retrived after signing up at https://cocreate.app             
+    host: '' // Points to socket server. If not defined it will use the url in web browser by default
+}; 
