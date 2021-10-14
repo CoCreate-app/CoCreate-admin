a32eed51751f74f6263fa9fb7eaab4ed14b67695
Frank Pagan <frank@cocreate.app>
2021-10-14 18:07:35 +0000

    fix: added notification template to index.html

---
+++ src/admin/apikey.js
@@ -0,0 +1,21 @@
+             
+    var config = {                 
+        apiKey: '2061acef-0451-4545-f754-60cf8160',                 
+        organization_Id: '5ff747727005da1c272740ab',                 
+        host: 'general.cocreate.app'
+    } 
+    
+    // Mo
+    // var config = {                 
+    //     apiKey: 'c05a5bfe-0fe1-44c9-cf48-fed9f835',                 
+    //     organization_Id: '60fa0259a701eb7f5117e4c8',                 
+    //     // host: 'general.cocreate.app'  
+    //     host: 'server.cocreate.app:8088'
+    // }         
+    
+    // var config = {
+    //     apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
+    //     organization_Id: '5de0387b12e200ea63204d6c',
+    //     host: 'wss://server.cocreate.app:8088'
+    //  }
+    
