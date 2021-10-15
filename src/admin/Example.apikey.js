8c5f4ef0dbcffdeab30b5b2583ef0299a3d9f301
Frank Pagan <frank@cocreate.app>
2021-10-15 00:11:39 +0000

    fix: update apiKeys

--- src/admin/Example.apikey.js
+++
@@ -1,5 +0,0 @@
-var config = {                 
-    apiKey: '', // Your organization_id can be retrived after signing up at https://cocreate.app              
-    organization_Id: '', // Your organization_id can be retrived after signing up at https://cocreate.app             
-    host: '' // Points to socket server. If not defined it will use the url in web browser by default
-}; 
