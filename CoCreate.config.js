1090ca358a01c6ee568c5856c3f5f97116daf598
Frank <frank@cocreate.app>
2022-02-28 21:37:16 +0000

    fix: bump cdn version

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "config": {
         "apiKey": "2061acef-0451-4545-f754-60cf8160",
         "organization_Id": "5ff747727005da1c272740ab",
-        "host": "server.cocreate.app"
+        "host": "general.cocreate.app"
     },
     "sources": [
         {
