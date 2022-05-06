2eaa2237074b299a550e3cc29470cd1c0acf6ba9
Frank Pagan <frank@cocreate.app>
2022-05-06 20:45:56 +0000

    fix: update config organization_Id to organization_id

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "config": {
         "apiKey": "2061acef-0451-4545-f754-60cf8160",
-        "organization_Id": "5ff747727005da1c272740ab",
+        "organization_id": "5ff747727005da1c272740ab",
         "host": "general.cocreate.app"
     },
     "sources": [
--- src/admin/apikey.js
+++ src/admin/apikey.js
@@ -5,7 +5,7 @@
 **/
 var config = {                 
     apiKey: '2061acef-0451-4545-f754-60cf8160', // Your apiKey can be retrived after signing up at https://cocreate.app              
-    organization_Id: '5ff747727005da1c272740ab', // Your organization_id can be retrived after signing up at https://cocreate.app              
+    organization_id: '5ff747727005da1c272740ab', // Your organization_id can be retrived after signing up at https://cocreate.app              
     host: 'general.cocreate.app' // Points to socket server. If not defined it will use the url in web browser by default
 } 
     
