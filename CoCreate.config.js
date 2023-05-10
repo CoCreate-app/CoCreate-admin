5ba5827b83da3377027706d185ea4b1f5a30f4ca
frankpagan <frank@cocreate.app>
2023-05-10 15:57:15 -0600

    git commit -am "fix: renamed apikey to key, bump cdn to 1.54.2"

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "config": {
         "organization_id": "5ff747727005da1c272740ab",
-        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "key": "2061acef-0451-4545-f754-60cf8160",
         "host": "general.cocreate.app"
     },
     "directories": [
--- src/apikey.js
+++ src/apikey.js
@@ -3,7 +3,7 @@
 * inorder to sync databases
 **/
 var CoCreateConfig = {                 
-    apiKey: '', // Your apiKey can be retrived from your browsers localStorage           
+    key: '', // Your key can be retrived from your browsers localStorage           
     organization_id: '', // Your organization_id can be retrived from your browsers localStorage       
     host: '', // Points to socket server. If not defined it will use the url in web browser by default
     serviceWorker: '/sw.js' 
