0d6d56bd6b6334686a8ab8f3a7217cd5a2120798
frankpagan <frank@cocreate.app>
2023-05-16 16:17:21 -0600

    formating

--- src/apikey.js
+++ src/apikey.js
@@ -2,10 +2,9 @@
 * organization_id required if the files are servered over network using CoCreateWS
 * inorder to sync databases
 **/
-var CoCreateConfig = {                 
+var CoCreateConfig = {
     key: '', // Your key can be retrived from your browsers localStorage           
     organization_id: '', // Your organization_id can be retrived from your browsers localStorage       
     host: '', // Points to socket server. If not defined it will use the url in web browser by default
-    serviceWorker: '/sw.js' 
-} 
-    
+    serviceWorker: '/sw.js'
+}
