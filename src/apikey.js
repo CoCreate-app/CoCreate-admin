cbc457f10de7c2e67406c2fd1ce116cf09b247e8
frankpagan <frank@cocreate.app>
2023-08-15 13:56:48 -0600

    fix: updated serviceWorker path to /service-worker.js

--- src/apikey.js
+++ src/apikey.js
@@ -6,5 +6,5 @@ var CoCreateConfig = {
     key: '', // Your key can be retrived from your browsers localStorage           
     organization_id: '', // Your organization_id can be retrived from your browsers localStorage       
     host: '', // Points to socket server. If not defined it will use the url in web browser by default
-    serviceWorker: '/sw.js'
+    serviceWorker: '/service-worker.js'
 }
